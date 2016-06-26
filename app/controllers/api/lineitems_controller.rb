f143b7f2fa7e98880f414557e4c7e918e20fe887
Tom Lynam <tom.s.lynam@gmail.com>
2016-06-26 16:11:45 -0600

    add lineitem

--- app/controllers/api/lineitems_controller.rb
+++ app/controllers/api/lineitems_controller.rb
@@ -1,12 +1,26 @@
-class Api::LineItemsController < ApplicationController
+class Api::LineitemsController < ApplicationController
 
 	def index
-		render json: LineItems.all
+		render json: Lineitem.all
 	end
 
+	def new
+		@lineitem = Lineitem.new
+	end
+
+	def create
+		@lineitem = Lineitem.create(lineitem_params)
+		if @lineitem.save
+			render json: @lineitem
+		else
+			render json: {errors: @lineitem.errors.full_messages}
+		end
+	end
 
 	private
 
+		def lineitem_params
+			params.require(:lineitem).permit(:title, :price)
+		end
 
 end
-
--- webpack/components/LineItems.js
+++ webpack/components/LineItems.js
@@ -3,18 +3,68 @@ import React from 'react';
 class LineItems extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state = { lineitems: [] }
+		this.addLineItem = this.addLineItem.bind(this);
+	}
+
+	componentWillMount() {
+		$.ajax({
+			url: '/api/lineitems/',
+			type: 'GET',
+			dataType: 'JSON'
+		}).done( lineitems => {
+			this.setState({ lineitems });
+		}).fail( data => {
+			console.log(data);
+		})
+	}
+
+	addLineItem(e) {
+		// prevent default form submit from happening
+		e.preventDefault();
+		// make ajax call to create a new budget
+		let title = this.refs.title.value;
+		let price = this.refs.price.value;
+		$.ajax({
+			url: '/api/lineitems',
+			type: 'POST',
+			data: { lineitem: { title, price } },
+			dataType: 'JSON'
+		}).done( lineitem => {
+			this.setState({ lineitems: [lineitem, ...this.state.lineitems ]});
+			this.refs.addForm.reset();
+		}).fail( response => {
+			alert('did not work');
+		});
+	}
+
+	displayLineItems() {
+		return this.state.lineitems.map( lineitem => {
+			return( 
+	      <div key={`lineitem-${lineitem.id}`} className="col s12">
+	        <div className="card blue-grey darken-1">
+	          <div className="card-content white-text">
+	            <span className="card-title">{lineitem.title}: ${lineitem.price}</span>
+	          </div>
+	        </div>
+	      </div>
+			)
+		})
 	}
 
 	render() {
 		return(
 			<div>
-		    <form className="col s12">
+				<div className='row'>
+					{this.displayLineItems.bind(this)()}
+				</div>
+		    <form ref='addForm' onSubmit={this.addLineItem}>
 	        <div className="input-field col m6">
-	          <input id="title" type="text" required />
+	          <input ref="title" id="title" type="text" required />
 	          <label for="title">Expense/Credit</label>
 	        </div>
 	        <div className="input-field col m4">
-	          <input id="price" type="text" required />
+	          <input ref="price" id="price" type="text" required />
 	          <label for="price">Amount</label>
 	        </div>
 	        <div className="input-field col m2">
